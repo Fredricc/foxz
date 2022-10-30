d2134a9878d8a1d0f620dee92b6114ddb331fba8
Fredricc <kfred45.fk@gmail.com>
2022-10-30 05:01:47 +0300

    Component and Application Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [myDate, myDateUpdate ] = useState(new Date())
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      myDateUpdate(new Date());
+    }, 1000);
+  })
   return (
     <div> 
-    <h3>Example HTML</h3>
-    <input />
-    <div>${new Date()}</div>
+     <h3>Example React</h3>
+      <input />
+      <div>{myDate.toString()}</div>
     </div>
   );
 }
