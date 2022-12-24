33e249f4652700025eb3f99a0314232d0206f6ca
Fredricc <kfred45.fk@gmail.com>
2022-12-24 23:51:07 +0300

    Save

--- src/forms/TypeAheadActors.tsx
+++ src/forms/TypeAheadActors.tsx
@@ -1,7 +1,7 @@
 import { ReactElement, useState } from 'react';
 import { Typeahead } from 'react-bootstrap-typeahead'
 import { actorMovieDTO } from '../actors/actors.model'
-import { Id, name, character, picture } from '../actors/actors.model.js
+import { Id, name, character, picture } from '../actors/actors.model'
 
 export default function TypeAheadActors(props: typeAheadActorsProps) {
 
@@ -9,7 +9,7 @@ export default function TypeAheadActors(props: typeAheadActorsProps) {
         id: 1, name: 'Fredrick', character: '', picture: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Tom_Holland_by_Gage_Skidmore.jpg/220px-Tom_Holland_by_Gage_Skidmore.jpg'
     },
     {
-        id: 2, name: 'Duncan', character: '', picture: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Dwayne_Johnson_2%2C_2013.jpg/220px-Dwayne_Johnson_2%2C_2013.jpg'
+        id: 2, name: 'Dun', character: '', picture: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Dwayne_Johnson_2%2C_2013.jpg/220px-Dwayne_Johnson_2%2C_2013.jpg'
     },
     {
         id: 3, name: 'Luke', character: '', picture: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Meryl_Streep_December_2018.jpg/220px-Meryl_Streep_December_2018.jpg'
